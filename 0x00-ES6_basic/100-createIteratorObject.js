2ffa10badf3dae3e2c708a8ff454f9de6ee3fec6
Lynnmich <lynnenmichuki@gmail.com>
2023-07-24 16:01:27 +0000

    Advanced

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,9 @@
+export default function* createIteratorObject(report) {
+  const departments = Object.values(report.allEmployees);
+
+  for (const employeesList of departments) {
+    for (const employee of employeesList) {
+      yield employee;
+    }
+  }
+}
