96b1c7d6c328a8543adf267b873d9201e4ecad5e
Lynnmich <lynnenmichuki@gmail.com>
2023-07-28 13:33:22 +0000

    Task 11

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,17 @@
+import Car from './10-car';
+
+class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._motor = motor;
+    this._color = color;
+    this._range = range;
+  }
+
+  cloneCar() {
+    const NewCar = super.constructor || super[Symbol.species];
+    return new NewCar();
+  }
+}
+
+export default EVCar;
