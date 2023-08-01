2459c3f8414156830ad5bbe8505be67b56d1b4d3
Lynnmich <lynnenmichuki@gmail.com>
2023-08-01 12:09:06 +0000

    Task 6

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,11 +1,9 @@
 export default function updateStudentGradeByCity(studentList, city, newGrades) {
-  return studentList.filter((element) => element.location === city).map((element) => {
-    element.grade = 'N/A';
-    for (const grade of newGrades) {
-      if (grad.studentId === element.id) {
-        element.grade = grad.grade
-      }
-    }
-    return element;
-  });
-}
+  return students.map((student) => {
+    const matchedGrade = newGrades.find((grade) => grade.studentId === student.id);
+    return {
+      ...student,
+      grade: matchedGrade ? matchedGrade.grade : "N/A",
+    };
+  }).filter((student) => student.location === city);
+};
