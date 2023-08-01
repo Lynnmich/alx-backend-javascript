08eeeb68ad77f05026910ebe2f5acafffc167a92
Lynnmich <lynnenmichuki@gmail.com>
2023-08-01 12:25:40 +0000

    Task 8

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,8 +1,3 @@
 export default function hasValuesFromArray(set, array) {
-  for (const propOfArray) {
-    if (!set.has(prop)) {
-      return false;
-    }
-  }
-  return true;
-}
+  return array.every((element) => set.has(element));
+};
