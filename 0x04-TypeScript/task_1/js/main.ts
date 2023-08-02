f38a84b7a40e6811be75f1362a5798425758a6d5
Lynnmich <lynnenmichuki@gmail.com>
2023-08-02 10:49:59 +0000

    Task 1 corrected

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -6,3 +6,41 @@ interface Teacher {
   location: string;
   [key: string]: any;
 }
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+function printTeacher(firstName: string, lastName: string) {
+  return `${firstName[0]}. ${lastName}`
+}
+
+interface printTeacherFunction {
+  firstName: string;
+  lastName: string;
+}
+
+interface classInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements classInterface {
+  firstName: string;
+  lastName: string;
+    
+  constructor(firstName: string, lastName: string){
+    this.firstName = firstName;
+    this.lastName = lastName;
+        
+  }
+  workOnHomework() {
+    return 'Currently working';
+  }
+  displayName() {
+    return `${this.firstName}`
+  }
+}
+
