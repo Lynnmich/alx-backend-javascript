e78898e0632a5234f999732b4623ff7c874cf19a
Lynnmich <lynnenmichuki@gmail.com>
2023-09-22 09:42:59 +0300

    Tasks 5 and 7

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,68 +1,29 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
 
-const path = process.argv[2];
+const args = process.argv.slice(2);
+const countStudents = require('./3-read_file_async');
+
+const DATABASE = args[0];
+
+const app = express();
 const port = 1245;
 
-const countStudents = (path) => {
-  const promise = (res, rej) => {
-    fs.readFile(path, 'utf8', (err, resData) => {
-      if (!err) {
-        const printOut = [];
-        let printItem; // item to printed
-        const data = resData.toString().split('\n');
-        let students = data.filter((item) => item);
-        students = students.map((item) => item.split(','));
-        printItem = `Number of students: ${students.length - 1}`;
-        console.log(printItem);
-        printOut.push(printItem);
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
 
-        const fields = {};
-        for (const student in students) {
-          if (student !== 0) {
-            if (!fields[students[student][3]]) {
-              fields[students[student][3]] = [];
-            }
-            fields[students[student][3]].push(students[student][0]);
-          }
-        }
-        delete fields.field;
-        for (const key of Object.keys(fields)) {
-          printItem = `Number of students in ${key}: ${
-            fields[key].length}. List: ${fields[key].join(', ')}`;
-          console.log(printItem);
-          printOut.push(printItem);
-        }
-        res(printOut);
-      } else {
-        rej(new Error('Cannot load the database'));
-      }
-    });
-  };
-
-  return new Promise(promise);
-};
-
-const app = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello Holberton School!');
-  }
-  if (req.url === '/students') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    countStudents(path)
-      .then((data) => {
-        res.end(`This is the list of our students\n${data.join('\n')}`);
-      })
-      .catch((error) => {
-        res.end(error);
-      });
+app.get('/students', async (req, res) => {
+  const msg = 'This is the list of our students\n';
+  try {
+    const students = await countStudents(DATABASE);
+    res.send(`${msg}${students.join('\n')}`);
+  } catch (error) {
+    res.send(`${msg}${error.message}`);
   }
 });
 
 app.listen(port, () => {
+  //   console.log(`Example app listening at http://localhost:${port}`);
 });
 
 module.exports = app;
