532a394f419ba3219e566d0668dd7fb186108812
Lynnmich <lynnenmichuki@gmail.com>
2023-09-22 10:01:57 +0300

    Task 0

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -5,13 +5,13 @@ describe('calculateNumber', () => {
     it('rounding of a', () => {
         assert.equal(calculateNumber(16.1, 2), 18);
         assert.equal(calculateNumber(2.2, 0), 2);
-        assert.equal(calculateNumber(3.1, 3), 6);
+        assert.equal(calculateNumber(3.1, 2.5), 6);
     });
 
     it('rounding of b', () => {
         assert.equal(calculateNumber(2, 16.1), 18);
         assert.equal(calculateNumber(0, 2.2), 2);
-        assert.equal(calculateNumber(3, 3.1), 6);
+        assert.equal(calculateNumber(2.5, 3.1), 6);
     });
 
     it('suming of a and b', () => {
